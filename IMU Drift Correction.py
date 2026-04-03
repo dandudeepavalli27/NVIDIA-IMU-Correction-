8caee0ac2505e5ddcc0f17d01d58ea05a4be94dd
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 12:32:37 +0530

    Create IMU Drift Correction.py

---
+++ IMU Drift Correction.py
@@ -0,0 +1,14 @@
+# SESSION 39 – IMU Drift Correction
+
+# Step 1: Input values
+orientation = 30    # original orientation (degrees)
+drift = 1.8         # drift error
+
+# Step 2: Apply correction
+corrected_orientation = orientation - drift
+
+# Step 3: Display result
+print("Corrected Orientation =", corrected_orientation)
+print("Orientation Stabilized")
+
+print("\nProgram Executed Successfully")
